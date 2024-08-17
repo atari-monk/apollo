4daebefc606547acc587f95bbce72f20b1ffcc1b
atari-monk <atari.monk1@gmail.com>
2024-08-17 19:10:45 +0200

    fix sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -9,7 +9,7 @@ function addLinks() {
   const pathSegments = currentPath.split('/').filter(Boolean)
 
   const lastSegment = pathSegments[pathSegments.length - 1]
-  const filePattern = /^file\d{3}$/
+  const filePattern = /^file\d{3}\.html$/
 
   if (filePattern.test(lastSegment)) {
     const baseIndexPath = pathSegments.slice(0, -1).join('/')
