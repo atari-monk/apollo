60af536d3fbbdc8e5b0e802d53e72b97cc08c373
atari-monk <atari.monk1@gmail.com>
2024-08-17 18:59:59 +0200

    fix sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,16 +1,15 @@
 function addLinks() {
   const sidebar = document.getElementById('nav-links')
   const currentPath = window.location.pathname
+
   const homeLink =
     '<li><a href="https://atari-monk.github.io/apollo/">Home</a></li>'
   sidebar.innerHTML = homeLink
 
   const pathSegments = currentPath.split('/').filter(Boolean)
-  const apolloIndex = pathSegments.indexOf('apollo')
-  const relevantSegments = pathSegments.slice(apolloIndex + 1)
 
-  if (relevantSegments.length > 0) {
-    const baseIndexPath = relevantSegments.join('/')
+  if (pathSegments.length > 2) {
+    const baseIndexPath = pathSegments.slice(0, -1).join('/')
     const indexLink = `<li><a href="https://atari-monk.github.io/${baseIndexPath}/">Current Folder Index</a></li>`
     sidebar.innerHTML += indexLink
   }
